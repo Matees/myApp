5759dbabfa3eebff8aff6ef4eb591475aa5119e5
Matej <you@example.com>
2017-05-01 20:20:40 +0200

    prvy commit

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,5 +1,14 @@
-c. <!-- Bootstrap core CSS -->
+<!DOCTYPE html>
 <link href="<?php echo
 base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
 <!-- Bootstrap theme -->
-<link href="<?php echo base_url();
+<link href="<?php echo base_url();?>assets/css/bootstrap-theme.min.css"
+      rel="stylesheet">
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+</head>
